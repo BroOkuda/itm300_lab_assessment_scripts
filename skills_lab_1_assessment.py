d1c44f32884804bf8d6008b912105cb24ba939db
BroOkuda <okudam@byui.edu>
2026-06-08 15:54:00 -0600

    Updated the setup instruction

--- skills_lab_1_assessment.py
+++ skills_lab_1_assessment.py
@@ -14,16 +14,19 @@ In the termianl / PowerShell of your computer
   - cat > ~/.aws/credentials
   (paste the content and hit a return key)
   CTRL+D (press CTRL and D keys together)
+- Install rich module if you have not installed it yet
+  - python3 -m venv venv
+  - pip install --upgrade pip
+  - pip install rich
 - Run this Python script
-  - python skills_lab_1_assessment.py  or  python3 skills_lab_1_assessment.py
+  - python3 skills_lab_1_assessment.py
   Or 
   - load this script in VS Code and run it
 - Run this Pythong script and direcct the output to both the screen and a file
   On bash
-    - python skills_lab_1_assessment.py | tee output.txt
+    - python3 skills_lab_1_assessment.py | tee output.txt
   On PowerShell
-    - python script.py | Tee-Object -FilePath "output.txt"
-
+    - python3 script.py | Tee-Object -FilePath "output.txt"
 '''
 
 
